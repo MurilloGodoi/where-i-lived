038bdb6a91f96d6566b1660b6a870a4398f1026f
Murillo <murillogodoi13@hotmail.com>
2021-08-19 18:21:01 -0300

    Initial commit from Create Next App

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,17 @@
+import * as S from './styles'
+const Main = () => (
+  <S.Wrapper>
+    <S.Logo
+      src="https://media-exp1.licdn.com/dms/image/C4D03AQElO6rIQRQ71Q/profile-displayphoto-shrink_100_100/0/1597885543585?e=1634774400&v=beta&t=v3olPT2vT4CyMIlOYIlWM89ccULQkzAAyCfD5CAwQNE"
+      alt="Foto de Murillo Godoi"
+    />
+    <S.Title>Murillo Godoi</S.Title>
+    <S.Description>Desenvolvedor Fullstack</S.Description>
+    <S.Illustration
+      src="https://www.luiztools.com.br/wp-content/uploads/2020/06/reactJS.png"
+      alt="Uma logo do ReactJS"
+    />
+  </S.Wrapper>
+)
+
+export default Main
